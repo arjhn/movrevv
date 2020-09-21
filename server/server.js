ee0b1657a664aafd71a2a1d0fcc39f5fe659b99e
Kanthi Kiran <arjhn@localhost.localdomain>
2020-09-21 18:29:29 +0530

    glitch check2

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.post('/postreview',(req,res)=>{
 app.get('/getAll',(req,res)=>{
     let dataObj=JSON.
         parse(fileR.readFileSync('./data.json'))
-        console.log('adjajdka')
+        console.log('adjajdkadadaa')
     res.send(dataObj['data'])
 
 })
